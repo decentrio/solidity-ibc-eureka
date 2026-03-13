55c6afcdde5103cfbf47d78f5ab1fa17a709d698
anhductn2001 <ntd1312000@gmail.com>
2026-03-13 15:27:33 +0700

    fix: use i++ instead of ++i in for loops to match minh/relayer

--- contracts/light-clients/SP1ICS07Tendermint.sol
+++ contracts/light-clients/SP1ICS07Tendermint.sol
@@ -341,7 +341,7 @@ contract SP1ICS07Tendermint is
         // loop through the key-value pairs and validate them
         {
             bool found = false;
-            for (uint256 i = 0; i < output.kvPairs.length; ++i) {
+            for (uint256 i = 0; i < output.kvPairs.length; i++) {
                 if (!Paths.equal(output.kvPairs[i].path, kvPath)) {
                     continue;
                 }
@@ -532,7 +532,7 @@ contract SP1ICS07Tendermint is
     /// @dev WARNING: Transient store is not reverted even if a message within a transaction reverts.
     /// @dev WARNING: This function must be called after all proof and validation checks.
     function _cacheKvPairs(uint64 proofHeight, IMembershipMsgs.KVPair[] memory kvPairs, uint256 timestamp) private {
-        for (uint256 i = 0; i < kvPairs.length; ++i) {
+        for (uint256 i = 0; i < kvPairs.length; i++) {
             bytes32 kvPairHash = keccak256(abi.encode(proofHeight, kvPairs[i]));
             kvPairHash.asUint256().tstore(timestamp);
         }
